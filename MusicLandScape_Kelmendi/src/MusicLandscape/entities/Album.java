94e65bb6a2237069e47591390d2444102e35a0fc
altin <kelmendialtin48@gmail.com>
2022-05-13 19:56:11 +0200

    New Classes+ Comparators +ET

--- MusicLandScape_Kelmendi/src/MusicLandscape/entities/Album.java
+++ MusicLandScape_Kelmendi/src/MusicLandscape/entities/Album.java
@@ -1,7 +1,63 @@
 package MusicLandscape.entities;
 
 public class Album extends Release{
-public TrackListItem trackListHead;
+public TrackListItem trackListHead ;
+
+Album(){
+
+}
+
+Album(Album other){
+    this.trackListHead=other.trackListHead;
+}
+
+Album(String title, Artist artist, int year){
+    Track t=new Track(title);
+    t.setPerformer(artist);
+    t.setYear(year);
+    trackListHead=new TrackListItem(t);
+}
+
+public boolean addTrack(Track track){
+    TrackListItem neu = new TrackListItem(track);
+
+    if(trackListHead== null){
+        trackListHead = neu;
+    }
+    else{
+        TrackListItem current = trackListHead;
+        while(current.next != null) {
+            current = current.next;
+        }
+        current.next = neu;
+    }
+    return true;
+}
+
+public Track removeTrack(int idx){
+    TrackListItem x =trackListHead;
+    for(int i=0; i < idx-1; i++){
+        x = x.next;
+    }
+    TrackListItem y =trackListHead;
+    for(int i=0; i < idx+1; i++){
+        y = y.next;
+    }
+    TrackListItem z =trackListHead;
+    for(int i=0; i < idx; i++){
+        z = z.next;
+    }
+    Track deleted =new Track(z.track);
+
+    if(idx==0){
+        trackListHead=trackListHead.next;
+    }
+    else{
+        x.next=y;
+    }
+
+    return deleted;
+}
 
     @Override
     int totalTime() {
---
+++ MusicLandScape_Kelmendi/src/MusicLandscape/entities/TrackListItem.java
@@ -0,0 +1,10 @@
+package MusicLandscape.entities;
+
+public class TrackListItem {
+TrackListItem next;
+Track track;
+
+TrackListItem(Track track){
+    TrackListItem.track=track;
+}
+}
